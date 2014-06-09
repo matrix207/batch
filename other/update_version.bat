e43363711cd5a174ac3b4a0fc18955794d6ea24b
Dennis <dennis.cpp@gmail.com>
2014-06-09 16:54:34 +0800

    Update update_version.bat

--- other/update_version.bat
+++ other/update_version.bat
@@ -1,9 +1,19 @@
 @echo off&setlocal enabledelayedexpansion
 
-REM 功能: 自动更新软件版本编译时间
+REM 功能: 自动更新软件版本文件version.h的编译时间
+REM       Linux下的做法如下 update-version.sh
+REM 	  sh -x -c "sed -i -e '/__SOFTWARE_AUTO_VERSION__/ {
+REM	  	s/=.*$/= \"svn-r$(svnversion), build-$(date '+%Y-%m-%d %H:%M')\"\;/
+REM 	  }' version.h"
+REM 	  sh -x -c "sed -i -e '/__SOFTWARE_AUTO_DATE__/ {
+REM  		s/=.*$/= \"$(date '+%Y')\"\;/
+REM 	  }' version.h"
 REM 参考: http://stackoverflow.com/a/20227248
-REM 问题: 结果文件中会删除!符号
-REM 2014-06-09 Dennis Create
+REM 问题: 
+REM       1. 结果文件中会删除!符号
+REM       2. for loop 中的 findstr 命令执行影响性能
+REM 历史:
+REM       2014-06-09 Dennis Create
 
 set yy=%date:~0,4%
 set mm=%date:~5,2%
@@ -25,8 +35,8 @@ for /f "tokens=1,* delims=" %%a in ( '"findstr /n ^^ %file%.tmp"') do (
 	if  "!line!" == "" (
 		echo.>> %file%.ttt
 	) else (
-      REM SET modified=!line:%SEARCHTEXT%=%REPLACETEXT%!
-		REM echo "!line!" | findstr /C:"define DATE_TIME" > nul
+		REM SET modified=!line:%SEARCHTEXT%=%REPLACETEXT%!
+		echo "!line!" | findstr /C:"define DATE_TIME" > nul
 		if !errorlevel! == 1 (
 			echo Found ***** !line!
 			set flag=1
